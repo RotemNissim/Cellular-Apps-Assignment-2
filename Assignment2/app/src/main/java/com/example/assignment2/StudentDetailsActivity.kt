b8714b6a89f3402c5a258fe4ae85e99a2737fe5a
RotemNissim <rotemnissim28@gmail.com>
2025-01-18 18:29:54 +0200

    fixing

--- Assignment2/app/src/main/java/com/example/assignment2/StudentDetailsActivity.kt
+++ Assignment2/app/src/main/java/com/example/assignment2/StudentDetailsActivity.kt
@@ -1,5 +1,11 @@
 package com.example.assignment2
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 class StudentDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- Assignment2/app/src/main/java/com/example/assignment2/StudentListActivity.kt
+++ Assignment2/app/src/main/java/com/example/assignment2/StudentListActivity.kt
@@ -1,5 +1,11 @@
 package com.example.assignment2
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 class StudentListActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
