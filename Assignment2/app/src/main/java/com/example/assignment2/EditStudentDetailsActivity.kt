09151c9a2fb4333537a44581da4b3e9816a4e227
RotemNissim <rotemnissim28@gmail.com>
2025-01-20 16:10:50 +0200

    Update EditStudentDetailsActivity.kt

--- Assignment2/app/src/main/java/com/example/assignment2/EditStudentDetailsActivity.kt
+++ Assignment2/app/src/main/java/com/example/assignment2/EditStudentDetailsActivity.kt
@@ -8,12 +8,22 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.assignment2.data.StudentRepository
+import android.view.MenuItem
 
 class EditStudentActivity : AppCompatActivity() {
 
     private var studentIndex: Int = -1
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_student)
 
